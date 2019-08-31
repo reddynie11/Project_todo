82e1795798b267e60f1514568cfd3bd02dce7fce
Narasimha Reddy <reddy.nie11@gmail.com>
2019-08-31 19:20:17 +0530

    'react-router-dom'

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <h1>Home</h1>
-    </div>
+    </BrowserRouter>
   );
 }
 
