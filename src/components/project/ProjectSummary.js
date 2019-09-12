6d82e284dac46308f0d4de4db5c8e997e2c490ec
Narasimha Reddy <reddy.nie11@gmail.com>
2019-09-12 15:35:59 +0530

    'ProjectSummary'

---
+++ src/components/project/ProjectSummary.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ProjectSummary = ()=>{
+    return(
+        <div className="card z-depth-0 project-summary">
+            <div className="card-content grey-text text-darken-3">
+                <span className="card-title">Notes Title</span>
+                <p>added by NR</p>
+                <p className="grey-text">Sep 3rd, 2019 at 10.00am</p>
+            </div>
+        </div>
+    )
+}
+export default ProjectSummary;
