5d63475002a710fa03d54d2e4c9090c7e16c44bf
Narasimha Reddy <reddy.nie11@gmail.com>
2019-09-18 21:08:33 +0530

    'connect_redux_firebase'

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './store/reducers/rootReducer';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
+import {reduxFirestore, getFirestore} from 'redux-firestore';
+import {reactReduxFirebase, getFirebase} from 'react-redux-firebase';
+import fbconfig from './config/fbconfig';
 
 
-const Store = createStore(rootReducer, applyMiddleware(thunk));
+const Store = createStore(rootReducer, 
+    compose(applyMiddleware(thunk.withExtraArgument({ getFirebase ,getFirestore})),
+    reduxFirestore(fbconfig),
+    reactReduxFirebase(fbconfig)
+    ));
 
 
 ReactDOM.render(<Provider store={Store} > <App /> </Provider>  , document.getElementById('root'));
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,5 +1,5 @@
 export const createProject = (project)=>{
-    return (dispatch, getState)=>{
+    return (dispatch, getState, { getFirebase ,getFirestore})=>{
         // async code 
         dispatch({type:'CREATE_PROJECT', payload: project});
     }
