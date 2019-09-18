4bedd5d5a08eac5c697bc1cbf81a2ccd33cba699
Narasimha Reddy <reddy.nie11@gmail.com>
2019-09-18 14:12:37 +0530

    'signup'

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/project/ProjectDetails';
 import SignIn from './components/auth/SignIn';
-
+import SignUp from './components/auth/SignUp';
 
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
         <Route exact path="/" component={Dashboard} />
         <Route path="/project/:id" component={ProjectDetails} />
         <Route path="/signin" component={SignIn} />
-        
+        <Route path="/signup" component={SignUp} />
       </Switch>
     </BrowserRouter>
   );
